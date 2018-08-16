527330a3b75543b584a910ef3a34df0914d46045
Ashley Reese <ashley@victorianfox.com>
2018-08-16 13:56:33 +0200

    Move diff_base_game to tools.sh

--- tools.sh
+++ tools.sh
@@ -45,7 +45,7 @@ patch_source() {
   mkdir game/patched-${GAME_VERSION}
   cp -rp game/decompiled-${GAME_VERSION}/* game/patched-${GAME_VERSION}/
   cd game/patched-${GAME_VERSION}/
-  patch -p5 -l --binary < ../../patches/patches-${GAME_VERSION}-compile.patch
+  patch -p5 -l --binary --no-backup-if-mismatch < ../../patches/patches-${GAME_VERSION}-compile.patch
 
   [ $? -ne 0 ] && echo "Failed to patch source files!" && exit 1
 }
@@ -53,12 +53,24 @@ patch_source() {
 copy_to_src() {
   [ ! -d game/patched-${GAME_VERSION} ] && echo "Missing patched folder! Run patch_soruce first!" && exit 1
   [ -d src/game/java/com/cairn4 ] && echo "Target folder already exists, NOT running!" && exit 1
+  [ ! -f patches/patches-${GAME_VERSION}-api.patch ] && echo "Missing API patch file!" && exit 1
 
   mkdir -p src/game/java/com/cairn4
   cp -rp game/patched-${GAME_VERSION}/* src/game/java/com/cairn4
+  cd src/game/java/com/cairn4
+  patch -p5 -l --binary --no-backup-if-mismatch < ../../../../../patches/patches-${GAME_VERSION}-api.patch
 }
 
-pushd -n $(pwd)
+diff_base_game() {
+  [ ! -d src/game/java/com/cairn4 ] && echo "Target folder does not exist! Nothing to diff!" && exit 1
+  [ ! -d game/patched-${GAME_VERSION} ] && echo "Missing patched folder! Run patch_source first!" && exit 1
+
+  diff -rupb game/patched-${GAME_VERSION} src/game/java/com/cairn4 > patches/patches-${GAME_VERSION}-api.patch
+
+  [ $? -ne 1 ] && echo "Error creating a diff!" && exit 1
+}
+
+pushd -n $(pwd) >/dev/null
 
 echo "Running task: " $task
 
@@ -86,10 +98,13 @@ case "$task" in
     $0 copy_to_src
     ./gradlew idea
     ;;
+  "diff_base_game")
+    diff_base_game
+    ;;
   *)
     echo "Unknown task!"
     exit 1
     ;;
 esac
 
-popd
+popd >/dev/null
